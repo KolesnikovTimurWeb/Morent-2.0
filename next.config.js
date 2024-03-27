0adbe51b417fcaa078e216e0a2a0a3311f69b9bd
KolesnikovTimurWeb <109101206+KolesnikovTimurWeb@users.noreply.github.com>
2024-03-28 00:32:20 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,9 @@ module.exports = {
          "firebasestorage.googleapis.com"
       ],
    },
-   eslint: { 
-     ignoreDuringBuilds: ['/'], 
-   }, 
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
